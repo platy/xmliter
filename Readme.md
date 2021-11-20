b416aa829f04dbc3e9667aa2246a2907c466afd1
Mike Bush <platy@njk.onl>
2021-11-20 22:27:35 +0100

    Start implementing xmliter

--- Readme.md
+++ Readme.md
@@ -58,3 +58,125 @@ It's going to be diffing 2 different streams from 2 different threads, so maybe 
 
 * Consider a different parser, html5ever is doing a lot of things to be ideal to building a dom, but since I'm not building a dom it's probably sub-optimal. The main thing is the internalised strings, there is no need to do this as the number of strings I actually need at any time is pretty low, and allocations could be kept low by reusing strings and vecs of elements which have gone out of scope.
 * Consider switching to pull. Pulling instead of pushing might make chaining of transformations more ergonomic, it might also mean we can use a streaming iterator api. Pull would be much better for diff which wouldn't then need to synchronise 2 threads, it would just be one thread reading as needed from 2 iterators. On the other hand it makes it more difficult to split and produce 2 different collections from one source. But maybe that is easier to synchronise, if it was 2 threads building collections from one iterator, the first one to read just needs to leave the item borrowed for the second to use. Without a second thread stuff could also be collected (ie. using `Iterator::inspect`) but it wouldn't be able to use the same transformations and `FromIterator` for example.
+* To switch to an iterator and transformations, I need a single trait for the whole path including the final node and probably doctype etc too. Then a transformation will map to something which implements the same type. So it should be able to be totally lazy. Actually, if I was collecting more than one thing from a single iteration in rust, I wouldn't do it with transformations, I would do it imperatively with a for loop, some if's and matches.
+
+# htmliter
+
+Provides an iterator api for iterating over elements (and their context path) in html or xml documents. This can be used to perform transformations or extractions on documents without loading them into memory.
+
+The iterated item is a reference to a trait object which allows borrowing an xml/html item (element, text, cdata, ...) and all of it's ancestor elements. As the item is borrowed, it can't currently implement [`std::iter::Iterator`], but if borrow support is added to `Iterator` in the future, that will be added here and this could be used with for loops.
+
+The iterator chaining methods on `std::iter::Iterator` and the collectors which implement [`std::iter::FromIterator`] wouldn't be generally useful for iterating over these paths, so here we provide some iterator chaining methods and collectors, these would allow terse solutions for simple cases and pre-processing, but in more complex cases you are likely to be need to use `while let Some(item) = iter.next() {...}`.
+
+## Examples
+
+For simple examples you can use the chaining methods, but they obfuscate how a more complex problem would be solved, so both forms are shown here.
+
+### Remove everything except the main element
+
+```rust
+write!(&mut write, "{}", HtmlIter::from(read).include(css_selector!("main")));
+```
+
+```rust
+let mut iter = HtmlIter::from(read);
+while let Some(item) = iter.next() {
+    if let Some(under_main) = item.include(css_selector!("main")) { // anything without main in the path is ignored, and any context with main is stripped before the main
+        write!(&mut write, "{}", under_main)?
+    }
+}
+```
+
+### Remove everything with the class "bloat" under "main"
+
+```rust
+write!(&mut write, "{}", HtmlIter::from(read).exclude(css_selector!(("main") (."bloat"))));
+```
+
+```rust
+let mut iter = HtmlIter::from(read);
+while let Some(item) = iter.next() {
+    if !item.match_any(css_selector!(("main") (."bloat"))) { // if nothing in the item's path matches
+        write!(&mut write, "{}", item)?
+    }
+}
+```
+
+### Remove all the `id`s from the document
+
+```rust
+write!(&mut write, "{}", HtmlIter::from(read).map_all(|element| // on the iterator, `map_all` means that this mapping applies to every element in the document
+    element.map_attributes(|attributes| // to map the element, we map the attributes, meaning no allocations need to take place, and any elements later ignored don't actually need to be processed
+        attributes.filter(|name, _value|name != "id")))); // the attributes are a regular `std::iter::Iterator`
+```
+
+```rust
+let mut iter = HtmlIter::from(read);
+while let Some(item) = iter.next() {
+    let item = item.map_all(|element| // on the item, `map_all` means this mapping applies to every element in the path
+        element.map_attributes(|attributes|
+            attributes.filter(|name, _value|name != "id"))));
+    write!(&mut write, "{}", item)?
+}
+```
+
+### Extract all hyperlinks
+
+```rust
+let links: Vec<_> = HtmlIter::from(read).filter_map(|item| (item.name == "a").then(|| item.attr("href"))).collect(); // methods on the iterator which are identically named to those on `std::iter::Iterator` work in the expected way.
+```
+
+### Extract books expressed in RDFa
+
+Here is a more complex example combining both chaining transformers and an imperative loop. The construction of a `Book` involves storing selected bits of data while looping and, when complete constructing a complete object if possible. This is best achieved with an imperative loop and mutable state.
+
+```rust
+struct Book {
+    name: String,
+    description: Option<String>,
+    author: Option<String>
+}
+
+let books = HtmlIter::from(read)
+    // `group_under` gives us an item at this level for each mach in the document
+    .group_under(css_select!((["vocab"="https://schema.org/"] ["typeof"="Book"])))
+    // `filter_map` returns a regular iterator over `Book`
+    .filter_map(|book| {
+        let mut name = None;
+        let mut description = None;
+        let mut author = None;
+        // this while loop happens for all nodes under each book, ancestors coming before the grouping element have been stripped
+        while let Some(item) = book.next() {
+            // if this is a text node
+            if let Some(text) = item.text() {
+                // the first ancestor with a property attribute
+                match item.first_ancestor().attr("property") {
+                    Some("name") => name.extend(text),
+                    Some("description") => desription.extend(text),
+                    Some("author") => author.extend(text),
+                    None => {}
+                }
+            }
+        }
+        // book is complete, see if we have enough data collected
+        if !name.is_empty() {
+            Some(Book {
+                name,
+                description: !description.is_empty().then(|| description),
+                author: !author.is_empty().then(|| author),
+            })
+        } else {
+            None
+        }
+    });
+```
+
+## CSS-like Selectors
+
+The syntax of our selectors are inspired by CSS, to support CSS exactly we would need to use procedural macros, and I don't think this is a good enough reason to use them. The main changes are that identifiers are surround by strings (so that they can contain things not allowed in rust identifiers such as '-') and that groupings of selectors which are to match the same element are grouped with parens (because rust's tokeniser ignores whitespace). Mostly we're only supporting CSS level 1 - element names, ids, classes.
+
+Let me know if you have any better ideas.
+
+```
+div#main .bloat => ("div" #"main") (."bloat)
+```
--- src/lib.rs
+++ src/lib.rs
@@ -1,489 +1,91 @@
-use std::{borrow::Cow, fmt, io::Write, iter, mem};
+use std::{io::{self, BufRead, Cursor}};
 
-use html5ever::{
-    serialize::{self, Serializer},
-    tendril::StrTendril,
-    Attribute, QualName,
-};
+// pub mod selector;
 
-pub mod selector;
-mod traverser;
+// use selector::{ContextualSelector, Selector};
 
-use selector::{ContextualSelector, Selector};
-pub use traverser::*;
+pub trait HtmlIterator {
+    fn next(&mut self) -> Option<quick_xml::events::Event>;
 
-#[derive(Clone)]
-pub struct HtmlPathElement<'a, Handle> {
-    pub handle: Handle,
-    pub name: html5ever::QualName,
-    pub attrs: Cow<'a, [Attribute]>,
-}
+    // fn exclude(&self, selector: impl ContextualSelector) -> HtmlIter {
+    //     todo!()
+    // }
 
-impl<'a, Handle> HtmlPathElement<'a, Handle> {
-    pub fn attr(&self, name: QualName) -> Option<&StrTendril> {
-        self.attrs
-            .iter()
-            .find_map(|a| (a.name == name).then(|| &a.value))
-    }
+    // fn include(&self, selector: impl ContextualSelector) -> HtmlIter {
+    //     todo!()
+    // }
 
-    pub fn classes(&self) -> iter::Flatten<std::option::IntoIter<std::str::SplitWhitespace<'_>>> {
-        use html5ever::*;
-        const CLASS: QualName = QualName {
-            prefix: None,
-            ns: ns!(),
-            local: local_name!("class"),
-        };
-        self.attr(CLASS)
-            .map(|value| value.split_whitespace())
-            .into_iter()
-            .flatten()
-    }
-}
-
-impl<'a, Handle: fmt::Display> fmt::Display for HtmlPathElement<'a, Handle> {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "#{} <{}", self.handle, &*self.name.local)?;
-        for Attribute { name, value } in self.attrs.iter() {
-            write!(f, " {}=\"{}\"", &*name.local, value)?;
-        }
-        write!(f, ">")
-    }
-}
-
-pub type HtmlContext<'a, Handle> = &'a [HtmlPathElement<'a, Handle>];
-
-pub trait HtmlSink<Handle>: Sized
-where
-    Handle: Eq + Copy,
-{
-    type Output;
-
-    fn append_doctype_to_document(
-        &mut self,
-        name: &html5ever::tendril::StrTendril,
-        public_id: &html5ever::tendril::StrTendril,
-        system_id: &html5ever::tendril::StrTendril,
-    );
-
-    fn append_element(
-        &mut self,
-        context: HtmlContext<'_, Handle>,
-        element: &HtmlPathElement<'_, Handle>,
-    );
-
-    fn append_text(&mut self, context: HtmlContext<Handle>, text: &str);
-
-    fn reset(&mut self) -> Self::Output;
-
-    fn finish(mut self) -> Self::Output {
-        self.reset()
-    }
-}
-
-struct OpenElement<Handle> {
-    handle: Handle,
-    name: html5ever::QualName,
-}
-
-pub struct HtmlSerializer<Wr: Write, Handle> {
-    inner: html5ever::serialize::HtmlSerializer<Wr>,
-    open_element_path: Vec<OpenElement<Handle>>,
-}
-
-impl<Wr: Write, Handle: Eq + fmt::Display> HtmlSerializer<Wr, Handle> {
-    fn pop_to_path(&mut self, context: HtmlContext<'_, Handle>) {
-        assert!(context
-            .iter()
-            .zip(&self.open_element_path)
-            .all(|(a, b)| a.handle == b.handle));
-        if context.len() > self.open_element_path.len() {
-            panic!(
-                "Non-appended elements in context : {}",
-                context[self.open_element_path.len()..]
-                    .iter()
-                    .map(ToString::to_string)
-                    .collect::<String>()
-            );
-        }
-        while context.len() < self.open_element_path.len() {
-            let closed = self.open_element_path.pop().unwrap();
-            self.inner.end_elem(closed.name).unwrap();
+    fn write_into(mut self, f: impl io::Write) where Self: Sized {
+        let mut writer = quick_xml::Writer::new(f);
+        while let Some(event) = self.next() {
+            assert!(writer.write_event(&event).is_ok());
         }
     }
 
-    pub fn new(writer: Wr, opts: serialize::SerializeOpts) -> Self {
+    fn to_string(self) -> String where Self: Sized {
+        let mut buf = vec![];
+        self.write_into(Cursor::new(&mut buf));
+        String::from_utf8(buf).unwrap()
+    }
+}
+
+pub struct HtmlIter<B: BufRead> {
+    reader: quick_xml::Reader<B>,
+    read_buf: Vec<u8>,
+}
+
+impl<B: BufRead> HtmlIter<B> {
+    pub fn from_reader(reader: B) -> Self {
         Self {
-            inner: html5ever::serialize::HtmlSerializer::new(writer, opts),
-            open_element_path: vec![],
+            reader: quick_xml::Reader::from_reader(reader),
+            read_buf: Vec::new(),
         }
     }
 }
 
-impl<Wr: Write, Handle: Eq + Copy + fmt::Display> HtmlSink<Handle>
-    for &mut HtmlSerializer<Wr, Handle>
-{
-    type Output = ();
-
-    fn append_element(
-        &mut self,
-        context: HtmlContext<'_, Handle>,
-        element: &HtmlPathElement<'_, Handle>,
-    ) {
-        self.pop_to_path(context);
-
-        self.inner
-            .start_elem(
-                element.name.clone(),
-                element.attrs.iter().map(|att| (&att.name, &*att.value)),
-            )
-            .unwrap();
-        self.open_element_path.push(OpenElement {
-            handle: element.handle,
-            name: element.name.clone(),
-        });
-    }
-
-    fn append_text(&mut self, context: HtmlContext<Handle>, text: &str) {
-        self.pop_to_path(context);
-
-        self.inner.write_text(text).unwrap();
-    }
-
-    fn reset(&mut self) -> Self::Output {
-        self.pop_to_path(&[])
-    }
-
-    fn append_doctype_to_document(
-        &mut self,
-        name: &html5ever::tendril::StrTendril,
-        _public_id: &html5ever::tendril::StrTendril,
-        _system_id: &html5ever::tendril::StrTendril,
-    ) {
-        self.inner.write_doctype(name).unwrap()
-    }
-}
-
-pub struct ElementRemover<Handle: Eq + Copy, S: HtmlSink<Handle>, M: Selector> {
-    inner: S,
-    matcher: M,
-    skip_handle: Option<Handle>,
-}
-
-impl<Handle: Eq + Copy, S: HtmlSink<Handle>, M: Selector> ElementRemover<Handle, S, M> {
-    pub fn wrap(sink: S, matcher: M) -> Self {
-        Self {
-            inner: sink,
-            matcher,
-            skip_handle: None,
+impl<B: io::BufRead> HtmlIterator for HtmlIter<B> {
+    fn next(&mut self) -> Option<quick_xml::events::Event> {
+        self.read_buf.clear();
+        match self.reader.read_event(&mut self.read_buf) {
+            Ok(quick_xml::events::Event::Eof) => None,
+            Ok(e) => Some(e),
+            Err(e) => panic!("{}", e),
         }
-    }
-}
-
-impl<Handle: Eq + Copy, S: HtmlSink<Handle>, M: Selector> HtmlSink<Handle>
-    for ElementRemover<Handle, S, M>
-{
-    type Output = S::Output;
-
-    fn append_doctype_to_document(
-        &mut self,
-        name: &html5ever::tendril::StrTendril,
-        public_id: &html5ever::tendril::StrTendril,
-        system_id: &html5ever::tendril::StrTendril,
-    ) {
-        self.inner
-            .append_doctype_to_document(name, public_id, system_id)
-    }
-
-    fn append_element(
-        &mut self,
-        context: HtmlContext<'_, Handle>,
-        element: &HtmlPathElement<'_, Handle>,
-    ) {
-        if let Some(skip_handle) = self.skip_handle {
-            if context.iter().any(|elem| elem.handle == skip_handle) {
-                return;
-            } else {
-                self.skip_handle = None
-            }
-        }
-        let skip = self.matcher.context_match(context, element);
-        if skip {
-            self.skip_handle = Some(element.handle);
-            return;
-        }
-        self.inner.append_element(context, element)
-    }
-
-    fn append_text(&mut self, context: HtmlContext<Handle>, text: &str) {
-        if let Some(skip_handle) = self.skip_handle {
-            if context.iter().any(|elem| elem.handle == skip_handle) {
-                return;
-            } else {
-                self.skip_handle = None
-            }
-        }
-        self.inner.append_text(context, text)
-    }
-
-    fn reset(&mut self) -> Self::Output {
-        self.skip_handle = None;
-        self.inner.reset()
-    }
-}
-
-pub struct RootFilter<Handle: Eq + Copy, S: HtmlSink<Handle>, M: ContextualSelector, O = ()> {
-    inner: S,
-    matcher: M,
-    select_handle: Option<Handle>,
-    output: O,
-}
-
-impl<Handle: Eq + Copy, S: HtmlSink<Handle>, M: ContextualSelector, O: Default>
-    RootFilter<Handle, S, M, O>
-{
-    pub fn wrap(inner: S, matcher: M) -> Self {
-        Self {
-            inner,
-            matcher,
-            select_handle: None,
-            output: O::default(),
-        }
-    }
-}
-
-impl<Handle, S, M: ContextualSelector, O> HtmlSink<Handle> for RootFilter<Handle, S, M, O>
-where
-    Handle: Eq + Copy,
-    S: HtmlSink<Handle>,
-    O: Extend<S::Output> + Default,
-{
-    type Output = O;
-
-    fn append_doctype_to_document(
-        &mut self,
-        _name: &html5ever::tendril::StrTendril,
-        _public_id: &html5ever::tendril::StrTendril,
-        _system_id: &html5ever::tendril::StrTendril,
-    ) {
-    }
-
-    fn append_element(
-        &mut self,
-        context: HtmlContext<'_, Handle>,
-        element: &HtmlPathElement<'_, Handle>,
-    ) {
-        if let Some(select_handle) = self.select_handle {
-            if let Some(select_index) = context
-                .iter()
-                .enumerate()
-                .find_map(|(index, elem)| (elem.handle == select_handle).then(|| index))
-            {
-                // select continues
-                self.inner.append_element(&context[select_index..], element);
-                return;
-            } else {
-                // select ends
-                self.select_handle = None;
-                self.output.extend(iter::once(self.inner.reset()));
-            }
-        }
-        let select = self.matcher.context_match(context, element);
-        if select {
-            // select starts
-            let select_handle = element.handle;
-            self.inner.append_element(&[], element);
-            self.select_handle = Some(select_handle);
-        }
-    }
-
-    fn append_text(&mut self, context: HtmlContext<Handle>, text: &str) {
-        if let Some(select_handle) = self.select_handle {
-            if let Some(select_index) = context
-                .iter()
-                .enumerate()
-                .find_map(|(index, elem)| (elem.handle == select_handle).then(|| index))
-            {
-                // select continues
-                self.inner.append_text(&context[select_index..], text)
-            } else {
-                // select ends
-                self.select_handle = None;
-                self.output.extend(iter::once(self.inner.reset()));
-            }
-        }
-    }
-
-    fn reset(&mut self) -> Self::Output {
-        if self.select_handle.take().is_some() {
-            self.output.extend(iter::once(self.inner.reset()));
-            self.select_handle = None
-        }
-        mem::take(&mut self.output)
-    }
-}
-
-pub struct ElementSkipper<S, M> {
-    inner: S,
-    matcher: M,
-}
-
-impl<S, M: Selector> ElementSkipper<S, M> {
-    pub fn wrap(inner: S, matcher: M) -> Self {
-        Self { inner, matcher }
-    }
-}
-
-impl<Handle, S, M: Selector> HtmlSink<Handle> for ElementSkipper<S, M>
-where
-    Handle: Eq + Copy,
-    S: HtmlSink<Handle>,
-{
-    type Output = S::Output;
-
-    fn append_doctype_to_document(
-        &mut self,
-        _name: &html5ever::tendril::StrTendril,
-        _public_id: &html5ever::tendril::StrTendril,
-        _system_id: &html5ever::tendril::StrTendril,
-    ) {
-    }
-
-    fn append_element(
-        &mut self,
-        context: HtmlContext<'_, Handle>,
-        element: &HtmlPathElement<'_, Handle>,
-    ) {
-        if self.matcher.context_match(context, element) {
-            return;
-        }
-        // TODO optimise when not hitting
-        let filtered_path = context
-            .iter()
-            .filter(|element| !self.matcher.is_match(element))
-            .cloned()
-            .collect::<Vec<_>>();
-        self.inner.append_element(filtered_path.as_slice(), element);
-    }
-
-    fn append_text(&mut self, context: HtmlContext<Handle>, text: &str) {
-        // TODO optimise when not hitting
-        let filtered_path = context
-            .iter()
-            .filter(|element| !self.matcher.is_match(element))
-            .cloned()
-            .collect::<Vec<_>>();
-        self.inner.append_text(filtered_path.as_slice(), text);
-    }
-
-    fn reset(&mut self) -> Self::Output {
-        self.inner.reset()
-    }
-}
-
-impl<Handle: Copy + Eq, A: HtmlSink<Handle>, B: HtmlSink<Handle>> HtmlSink<Handle> for (A, B) {
-    type Output = (A::Output, B::Output);
-
-    fn append_doctype_to_document(
-        &mut self,
-        name: &html5ever::tendril::StrTendril,
-        public_id: &html5ever::tendril::StrTendril,
-        system_id: &html5ever::tendril::StrTendril,
-    ) {
-        self.0
-            .append_doctype_to_document(name, public_id, system_id);
-        self.1
-            .append_doctype_to_document(name, public_id, system_id);
-    }
-
-    fn append_element(
-        &mut self,
-        context: HtmlContext<'_, Handle>,
-        element: &HtmlPathElement<'_, Handle>,
-    ) {
-        self.0.append_element(context, element);
-        self.1.append_element(context, element);
-    }
-
-    fn append_text(&mut self, context: HtmlContext<Handle>, text: &str) {
-        self.0.append_text(context, text);
-        self.1.append_text(context, text);
-    }
-
-    fn reset(&mut self) -> Self::Output {
-        (self.0.reset(), self.1.reset())
     }
 }
 
 #[cfg(test)]
 mod test {
     use super::*;
-    use html5ever::{
-        serialize::{SerializeOpts, TraversalScope},
-        tendril::TendrilSink,
-        ParseOpts,
-    };
-
-    fn stream_doc(test: &str, sink: impl HtmlSink<u32>) {
-        let mut opts = ParseOpts::default();
-        opts.tree_builder.exact_errors = true;
-        let parser = parse_document(sink, opts);
-        parser.one(test);
-    }
-
-    fn serialiser(buf: &mut Vec<u8>) -> HtmlSerializer<&mut Vec<u8>, u32> {
-        let opts = SerializeOpts::default();
-        HtmlSerializer::new(buf, opts)
-    }
 
     #[test]
     fn doc_identity() {
-        let mut buf = Vec::new();
-        let mut sink = serialiser(&mut buf);
         let test = "<!DOCTYPE html><html><head></head><body><p><b>hello</b></p><p>world!</p></body></html>";
-        stream_doc(test, &mut sink);
-        assert_eq!(String::from_utf8(buf).unwrap(), test);
+        let out = HtmlIter::from_reader(test.as_bytes());
+        assert_eq!(&out.to_string(), test);
     }
 
     #[test]
-    // #[ignore = "html5ever mysteriously adds a <html> root"]
     fn fragment_identity() {
-        let mut buf = Vec::new();
-        let mut opts = SerializeOpts::default();
-        opts.traversal_scope = TraversalScope::ChildrenOnly(None);
-        let mut sink = HtmlSerializer::new(&mut buf, opts);
-        let mut opts = ParseOpts::default();
-        opts.tree_builder.exact_errors = true;
-        let parser = parse_fragment(&mut sink, opts);
         let test = "<p><b>hello</b></p><p>world!</p>";
-        parser.one(test);
-        assert_eq!(String::from_utf8(buf).unwrap(), test);
+        let out = HtmlIter::from_reader(test.as_bytes());
+        assert_eq!(&out.to_string(), test);
     }
 
-    #[test]
-    fn remove_elements() {
-        let mut buf = Vec::new();
-        let mut serializer = serialiser(&mut buf);
-        let test = r#"<!DOCTYPE html><html><head></head><body><p class="hello"><b>hello</b></p><p>world!</p></body></html>"#;
-        stream_doc(
-            test,
-            ElementRemover::wrap(&mut serializer, css_select!(."hello")),
-        );
-        assert_eq!(
-            String::from_utf8(buf).unwrap(),
-            r#"<!DOCTYPE html><html><head></head><body><p>world!</p></body></html>"#
-        );
-    }
+    // #[test]
+    // fn remove_elements() {
+    //     let test = r#"<!DOCTYPE html><html><head></head><body><p class="hello"><b>hello</b></p><p>world!</p></body></html>"#;
+    //     let out = HtmlIter::from_read(test.as_bytes()).exclude(css_select!(."hello"));
+    //     assert_eq!(
+    //         &out.to_string(),
+    //         r#"<!DOCTYPE html><html><head></head><body><p>world!</p></body></html>"#
+    //     );
+    // }
 
-    #[test] // for selection, a selected node needs to be appended to the document, if it is not already part of a selected tree. i think for this all to work, either each processor needs to have it's own traversal tree, or maybe, the traversal tree builder from a Sink is only the first step and the processing actually happens using a different interface, probably entirely triggered by appends, but also having a (filtered) access to the tracversal scope
-    fn select_element() {
-        let mut buf = Vec::new();
-        let mut serializer = serialiser(&mut buf);
-        let sink = RootFilter::<_, _, _>::wrap(&mut serializer, css_select!("p"));
-        let test = "<!DOCTYPE html><html><head></head><body><p><b>hello</b></p><p>world!</p></body></html>";
-        stream_doc(test, sink);
-        assert_eq!(buf, b"<p><b>hello</b></p><p>world!</p>");
-    }
-
-    #[test]
-    fn extract_data() {}
+    // #[test]
+    // fn select_element() {
+    //     let test = "<!DOCTYPE html><html><head></head><body><div id="main"><p><b>hello</b></p><p>world!</p></div><p>side</p></body></html>";
+    //     let out = HtmlIter::from_read(test.as_bytes()).include(css_select!((#"main" "p"));
+    //     assert_eq!(&out.to_string(), "<p><b>hello</b></p><p>world!</p>");
+    // }
 }
