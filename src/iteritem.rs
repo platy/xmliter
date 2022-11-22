d9d7cee8d816882e13ad7796a821eb61e576811f
Mike Bush <platy@njk.onl>
2022-11-22 04:47:44 +0100

    Extract element_path

--- src/iteritem.rs
+++ src/iteritem.rs
@@ -1,9 +1,10 @@
-use std::{fmt, io::BufRead, mem, slice::SliceIndex};
+use std::mem;
 
-use quick_xml::{
-    events::{BytesEnd, BytesStart, BytesText, Event},
-    Reader,
-};
+use quick_xml::events::{BytesEnd, BytesStart, BytesText, Event};
+
+mod element_path;
+
+pub use element_path::*;
 
 pub(crate) struct Traverser {
     buf: Vec<u8>,
@@ -62,204 +63,6 @@ impl Traverser {
     }
 }
 
-/// An owned path of elements
-#[derive(Debug, Clone)]
-pub struct ElementPathBuf {
-    path: Vec<NormalisedElement>,
-}
-
-impl ElementPathBuf {
-    pub(crate) fn new() -> Self {
-        Self { path: vec![] }
-    }
-
-    fn text(&self, text: String) -> Node {
-        Node::Text(text)
-    }
-
-    fn doctype(&self, text: String) -> Node {
-        Node::DocType(text)
-    }
-
-    fn start<B: BufRead>(&mut self, start: BytesStart, reader: &Reader<B>) -> Node {
-        let element = NormalisedElement {
-            name: reader.decode(start.name()).unwrap().to_string(),
-            attrs: start
-                .attributes()
-                .map(|a| {
-                    let a = a.unwrap();
-                    NormalisedAttribute {
-                        name: reader.decode(a.key).unwrap().to_string(),
-                        value: reader.decode(&a.value).unwrap().to_string(),
-                    }
-                })
-                .collect(),
-        };
-        self.path.push(element);
-        Node::Start
-    }
-
-    #[cfg(test)]
-    pub(crate) fn append_element(&mut self, name: &str, attr: Vec<(&str, &str)>) -> &mut Self {
-        let element = NormalisedElement {
-            name: name.to_string(),
-            attrs: attr
-                .into_iter()
-                .map(|(name, value)| NormalisedAttribute {
-                    name: name.to_string(),
-                    value: value.to_string(),
-                })
-                .collect(),
-        };
-        self.path.push(element);
-        self
-    }
-
-    fn end(&self) -> Node {
-        Node::End
-    }
-
-    pub(crate) fn as_path(&self) -> ElementPath {
-        ElementPath {
-            path: &self.path,
-            buf: self,
-        }
-    }
-}
-
-/// A path of elements
-#[derive(Clone, Copy)]
-pub struct ElementPath<'a> {
-    path: &'a [NormalisedElement],
-    buf: &'a ElementPathBuf,
-}
-
-impl<'a> ElementPath<'a> {
-    pub fn len(&self) -> usize {
-        self.path.len()
-    }
-
-    pub(crate) fn split_last(&self) -> Option<(RawElement<'a>, ElementPath<'a>)> {
-        if let Some((element, path)) = self.path.split_last() {
-            Some((
-                RawElement {
-                    element,
-                    _buf: self.buf,
-                },
-                ElementPath {
-                    path,
-                    buf: self.buf,
-                },
-            ))
-        } else {
-            None
-        }
-    }
-
-    pub(crate) fn as_item(&self) -> Option<RawItem<'a>> {
-        if !self.path.is_empty() {
-            Some(RawItem {
-                context: *self,
-                node: Node::Start,
-            })
-        } else {
-            None
-        }
-    }
-
-    fn as_element(&self, first: &'a NormalisedElement) -> RawElement<'a> {
-        RawElement {
-            element: first,
-            _buf: self.buf,
-        }
-    }
-
-    pub(crate) fn slice<I: SliceIndex<[NormalisedElement], Output = [NormalisedElement]>>(
-        &self,
-        index: I,
-    ) -> Self {
-        Self {
-            path: &self.path[index],
-            buf: self.buf,
-        }
-    }
-}
-
-impl<'a> fmt::Debug for ElementPath<'a> {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        for element in self.path {
-            write!(f, "/{:?}", element)?;
-        }
-        Ok(())
-    }
-}
-
-impl<'a> IntoIterator for ElementPath<'a> {
-    type Item = RawElement<'a>;
-
-    type IntoIter = ElementPathIter<'a>;
-
-    fn into_iter(self) -> Self::IntoIter {
-        ElementPathIter(self)
-    }
-}
-
-pub struct ElementPathIter<'a>(ElementPath<'a>);
-
-impl<'a> Iterator for ElementPathIter<'a> {
-    type Item = RawElement<'a>;
-
-    fn next(&mut self) -> Option<Self::Item> {
-        if let Some((first, rest)) = self.0.path.split_first() {
-            self.0.path = rest;
-            Some(self.0.as_element(first))
-        } else {
-            None
-        }
-    }
-}
-
-impl<'a> DoubleEndedIterator for ElementPathIter<'a> {
-    fn next_back(&mut self) -> Option<Self::Item> {
-        if let Some((last, rest)) = self.0.path.split_last() {
-            self.0.path = rest;
-            Some(self.0.as_element(last))
-        } else {
-            None
-        }
-    }
-}
-
-/// Currently Heap allocated, but to be fixed size with no references, instead should only contain slice index ranges into vecs stored on element paths
-#[derive(Clone)]
-pub(crate) struct NormalisedElement {
-    name: String,
-    attrs: Vec<NormalisedAttribute>,
-}
-
-impl fmt::Debug for NormalisedElement {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        f.write_str(&self.name)?;
-        for a in &self.attrs {
-            write!(f, " {}=\"{}\"", a.name, a.value)?;
-        }
-        Ok(())
-    }
-}
-
-/// Currently Heap allocated, but to be fixed size with no references, instead should only contain slice index ranges into vecs stored on element paths
-#[derive(Clone, Debug)]
-pub(crate) struct NormalisedAttribute {
-    pub(crate) name: String,
-    pub(crate) value: String,
-}
-
-/// An item in the traversal, with access to the current node and the context of elements
-pub struct RawItem<'a> {
-    pub(crate) context: ElementPath<'a>,
-    pub(crate) node: Node,
-}
-
 impl<'a> Item<'a> for RawItem<'a> {
     fn as_element(&self) -> Option<RawElement<'a>> {
         match self.node {
@@ -279,12 +82,6 @@ impl<'a> Item<'a> for RawItem<'a> {
 
     fn node(&self) -> &Node {
         &self.node
-    }
-}
-
-impl<'a> std::fmt::Debug for RawItem<'a> {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{:?}/{:?}", self.context, self.node)
     }
 }
 
@@ -379,38 +176,6 @@ where
 
     fn as_path(&self) -> ElementPath<'a> {
         todo!("ElementPath should be a trait")
-    }
-}
-
-// should index elements and unescaped text in the path. Wanted it to be private, maybe it still can be
-#[derive(Clone)]
-pub enum Node {
-    DocType(String),
-    Start,
-    End,
-    Text(String),
-}
-
-impl fmt::Debug for Node {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        match self {
-            Self::DocType(arg) => write!(f, "DOCTYPE {}", arg),
-            Self::Start => write!(f, "Start"),
-            Self::End => write!(f, "End"),
-            Self::Text(arg) => fmt::Debug::fmt(&arg, f),
-        }
-    }
-}
-
-/// An element in the context
-pub struct RawElement<'a> {
-    element: &'a NormalisedElement,
-    _buf: &'a ElementPathBuf,
-}
-
-impl<'a> RawElement<'a> {
-    pub(crate) fn attributes(&self) -> std::slice::Iter<'_, NormalisedAttribute> {
-        self.element.attrs.iter()
     }
 }
 
---
+++ src/iteritem/element_path.rs
@@ -0,0 +1,240 @@
+//! Core parts of representing an element and it's ancestral path
+use std::{fmt, io::BufRead, slice::SliceIndex};
+
+use quick_xml::{events::BytesStart, Reader};
+
+/// An owned path of elements
+#[derive(Debug, Clone)]
+pub struct ElementPathBuf {
+    pub(crate) path: Vec<NormalisedElement>,
+}
+
+impl ElementPathBuf {
+    pub(crate) fn new() -> Self {
+        Self { path: vec![] }
+    }
+
+    pub(crate) fn text(&self, text: String) -> Node {
+        Node::Text(text)
+    }
+
+    pub(crate) fn doctype(&self, text: String) -> Node {
+        Node::DocType(text)
+    }
+
+    pub(crate) fn start<B: BufRead>(&mut self, start: BytesStart, reader: &Reader<B>) -> Node {
+        let element = NormalisedElement {
+            name: reader.decode(start.name()).unwrap().to_string(),
+            attrs: start
+                .attributes()
+                .map(|a| {
+                    let a = a.unwrap();
+                    NormalisedAttribute {
+                        name: reader.decode(a.key).unwrap().to_string(),
+                        value: reader.decode(&a.value).unwrap().to_string(),
+                    }
+                })
+                .collect(),
+        };
+        self.path.push(element);
+        Node::Start
+    }
+
+    #[cfg(test)]
+    pub(crate) fn append_element(&mut self, name: &str, attr: Vec<(&str, &str)>) -> &mut Self {
+        let element = NormalisedElement {
+            name: name.to_string(),
+            attrs: attr
+                .into_iter()
+                .map(|(name, value)| NormalisedAttribute {
+                    name: name.to_string(),
+                    value: value.to_string(),
+                })
+                .collect(),
+        };
+        self.path.push(element);
+        self
+    }
+
+    pub(crate) fn end(&self) -> Node {
+        Node::End
+    }
+
+    pub(crate) fn as_path(&self) -> ElementPath {
+        ElementPath {
+            path: &self.path,
+            buf: self,
+        }
+    }
+}
+
+/// A path of elements
+#[derive(Clone, Copy)]
+pub struct ElementPath<'a> {
+    pub(crate) path: &'a [NormalisedElement],
+    pub(crate) buf: &'a ElementPathBuf,
+}
+
+impl<'a> ElementPath<'a> {
+    pub fn len(&self) -> usize {
+        self.path.len()
+    }
+
+    pub(crate) fn split_last(&self) -> Option<(RawElement<'a>, ElementPath<'a>)> {
+        if let Some((element, path)) = self.path.split_last() {
+            Some((
+                RawElement {
+                    element,
+                    _buf: self.buf,
+                },
+                ElementPath {
+                    path,
+                    buf: self.buf,
+                },
+            ))
+        } else {
+            None
+        }
+    }
+
+    pub(crate) fn as_item(&self) -> Option<RawItem<'a>> {
+        if !self.path.is_empty() {
+            Some(RawItem {
+                context: *self,
+                node: Node::Start,
+            })
+        } else {
+            None
+        }
+    }
+
+    pub(crate) fn as_element(&self, first: &'a NormalisedElement) -> RawElement<'a> {
+        RawElement {
+            element: first,
+            _buf: self.buf,
+        }
+    }
+
+    pub(crate) fn slice<I: SliceIndex<[NormalisedElement], Output = [NormalisedElement]>>(
+        &self,
+        index: I,
+    ) -> Self {
+        Self {
+            path: &self.path[index],
+            buf: self.buf,
+        }
+    }
+}
+
+impl<'a> fmt::Debug for ElementPath<'a> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        for element in self.path {
+            write!(f, "/{:?}", element)?;
+        }
+        Ok(())
+    }
+}
+
+impl<'a> IntoIterator for ElementPath<'a> {
+    type Item = RawElement<'a>;
+
+    type IntoIter = ElementPathIter<'a>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        ElementPathIter(self)
+    }
+}
+
+pub struct ElementPathIter<'a>(ElementPath<'a>);
+
+impl<'a> Iterator for ElementPathIter<'a> {
+    type Item = RawElement<'a>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if let Some((first, rest)) = self.0.path.split_first() {
+            self.0.path = rest;
+            Some(self.0.as_element(first))
+        } else {
+            None
+        }
+    }
+}
+
+impl<'a> DoubleEndedIterator for ElementPathIter<'a> {
+    fn next_back(&mut self) -> Option<Self::Item> {
+        if let Some((last, rest)) = self.0.path.split_last() {
+            self.0.path = rest;
+            Some(self.0.as_element(last))
+        } else {
+            None
+        }
+    }
+}
+
+/// Currently Heap allocated, but to be fixed size with no references, instead should only contain slice index ranges into vecs stored on element paths
+#[derive(Clone)]
+pub(crate) struct NormalisedElement {
+    pub(crate) name: String,
+    pub(crate) attrs: Vec<NormalisedAttribute>,
+}
+
+impl fmt::Debug for NormalisedElement {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.write_str(&self.name)?;
+        for a in &self.attrs {
+            write!(f, " {}=\"{}\"", a.name, a.value)?;
+        }
+        Ok(())
+    }
+}
+
+/// Currently Heap allocated, but to be fixed size with no references, instead should only contain slice index ranges into vecs stored on element paths
+#[derive(Clone, Debug)]
+pub(crate) struct NormalisedAttribute {
+    pub(crate) name: String,
+    pub(crate) value: String,
+}
+
+/// An item in the traversal, with access to the current node and the context of elements
+pub struct RawItem<'a> {
+    pub(crate) context: ElementPath<'a>,
+    pub(crate) node: Node,
+}
+
+impl<'a> std::fmt::Debug for RawItem<'a> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{:?}/{:?}", self.context, self.node)
+    }
+}
+
+// should index elements and unescaped text in the path. Wanted it to be private, maybe it still can be
+#[derive(Clone)]
+pub enum Node {
+    DocType(String),
+    Start,
+    End,
+    Text(String),
+}
+
+impl fmt::Debug for Node {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Self::DocType(arg) => write!(f, "DOCTYPE {}", arg),
+            Self::Start => write!(f, "Start"),
+            Self::End => write!(f, "End"),
+            Self::Text(arg) => fmt::Debug::fmt(&arg, f),
+        }
+    }
+}
+
+/// An element in the context
+pub struct RawElement<'a> {
+    pub(crate) element: &'a NormalisedElement,
+    pub(crate) _buf: &'a ElementPathBuf,
+}
+
+impl<'a> RawElement<'a> {
+    pub(crate) fn attributes(&self) -> std::slice::Iter<'_, NormalisedAttribute> {
+        self.element.attrs.iter()
+    }
+}
